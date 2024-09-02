3dfa3abc9fce4e8b05602beaa76a076dc61c08ea
KrokozavrCSharp <158212789+KrokozavrCSharp@users.noreply.github.com>
2024-09-02 09:53:28 +0500

    Update Counter.cs

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Counter : MonoBehaviour
 {
     private int _points;
-
     private float _declay = 0.5f;
-
-    private bool _isMouseClick;
-
+    private bool _isActivateCoroutine;
+    private KeyCode _leftMouseButton=KeyCode.Mouse0;
+    private Coroutine _coroutine;
+    
+    public event Action<int> PointsChanged;
+    
     public int Points => _points;
-
-    public event Action<int> PointsChange;
 
     private void Start()
     {
@@ -23,7 +23,7 @@ public class Counter : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKey(_leftMouseButton))
         {
             Click();
         }
@@ -33,7 +33,7 @@ public class Counter : MonoBehaviour
     {
         var wait = new WaitForSeconds(_declay);
 
-        while (_isMouseClick)
+        while (_isActivateCoroutine)
         {
             _points++;
 
@@ -45,15 +45,15 @@ public class Counter : MonoBehaviour
 
     private void Click()
     {
-        if(_isMouseClick==false)
+        if(_isActivateCoroutine==false)
         {
-            _isMouseClick = true;
-            StartCoroutine(Counting());
+            _isActivateCoroutine = true;
+            _coroutine=StartCoroutine(Counting());
         }
         else
         {
-            _isMouseClick = false;
-            StopCoroutine(Counting());
+            _isActivateCoroutine = false;
+            StopCoroutine(_coroutine);
         }
     }
 }
